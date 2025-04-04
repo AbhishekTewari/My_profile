f56ab51217bffcef66b2878cbc7abb179532b48a
AbhishekTewari <tiwariabhishek687@gmail.com>
2025-04-04 15:55:40 +0530

    changes in theme related to project page

--- wp-content/themes/my_profile/project-template.php
+++ wp-content/themes/my_profile/project-template.php
@@ -4,6 +4,7 @@
     */
 ?>
 <?php get_header(); ?>
+asdadsadaasd
     <?php 
        $categories = get_terms(array(
         'taxonomy'   => 'project_category', // Default WordPress category taxonomy
